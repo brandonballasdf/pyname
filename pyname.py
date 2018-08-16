91f5b2cfe83f77f70db13705ff4daa54fabc499d
brandonballasdf <35648735+brandonballasdf@users.noreply.github.com>
2018-08-16 14:22:21 -0400

    Add files via upload

--- pyname.py
+++ pyname.py
@@ -1,7 +1,7 @@
 import os
-import msvcrt as keypress
 
-name = input("Enter the name: ")
+print("Enter the name: ")
+name = input()
 path = os.getcwd()
 
 os.mkdir(os.path.join(path, name)) # make new directory
@@ -9,24 +9,27 @@ os.chdir(os.path.join(path, name)) # navigate into new directory
 
 path = os.getcwd() # update working directory
 
-print("Place media file and subtitle(optional) file in new folder folder\n")
+print("Place media file and subtitle(optional) file in new folder named " + name)
 input("Press Enter to continue...")
 
 filenames = os.listdir(path)
 
-for filename in filenames:
-    if filename[-4] == '.srt'
+
+for filename in filenames: # for loop searches for key file extensions and renames according to user input
+
+    if filename.endswith('.srt'):
         dst = name + '.srt'
         os.rename(filename, dst)
 
-    if filename[-4] == '.mp4'
+    elif filename.endswith('.mp4'):
         dst = name + '.mp4'
         os.rename(filename, dst)
 
-    if filename[-4] == '.avi'
+    elif filename.endswith('.avi'):
         dst = name + '.avi'
         os.rename(filename, dst)
 
 print("The following files have been renamed:\n\n")
 os.listdir()
-    
+
+return 0;
