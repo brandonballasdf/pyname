2d1aa1d31523901229a9019e1804b2fbdc2cd5ff
brandonballasdf <35648735+brandonballasdf@users.noreply.github.com>
2018-08-16 11:26:33 -0400

    Add files via upload

---
+++ pyname.py
@@ -0,0 +1,28 @@
+import os
+
+name = input("Enter the name: ")
+path = os.getcwd()
+
+os.mkdir(os.path.join(path, name)) # make new directory
+os.chdir(os.path.join(path, name)) # navigate into new directory
+
+path = os.getcwd() # update working directory
+
+filenames = os.listdir(path)
+
+for filename in filenames:
+    if filename[-4] == '.srt'
+        dst = name + '.srt'
+        os.rename(filename, dst)
+
+    if filename[-4] == '.mp4'
+        dst = name + '.mp4'
+        os.rename(filename, dst)
+
+    if filename[-4] == '.avi'
+        dst = name + '.avi'
+        os.rename(filename, dst)
+
+print("The following files have been renamed:\n\n")
+os.listdir()
+    
