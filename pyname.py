7e0b43adab0c9f458c71a0fe9efdff83871bf6d8
brandonballasdf <35648735+brandonballasdf@users.noreply.github.com>
2018-09-06 17:43:43 -0400

    Add files via upload
    Added tk gui for increased useability

--- pyname.py
+++ pyname.py
@@ -1,44 +1,61 @@
 import os
-
-print("Enter the name: ")
-name = input()
-path = os.getcwd()
-one = "1"
-
-hashed_name = one + name
-
-os.mkdir(os.path.join(path, hashed_name)) # make new directory
-os.chdir(os.path.join(path, hashed_name)) # navigate into new directory
-
-new_path = os.getcwd() # update working directory
-
-print("Place media file and subtitle(optional) file in new folder named " + name)
-input("Press Enter to continue...")
-
-filenames =  os.listdir(new_path)
+import subprocess
+from tkinter import *
 
 
-for filename in filenames: # for loop searches for key file extensions and renames according to user input
+def make_folder():
+    name_of_movie = movie.get()
+    path = os.getcwd()
 
-    if filename.endswith('.srt'):
-        dst = name + '.srt'
-        os.rename(filename, dst)
+    os.mkdir(os.path.join(path, name_of_movie)) # make new directory
+    os.chdir(os.path.join(path, name_of_movie)) # navigate into new directory
 
-    elif filename.endswith('.mp4'):
-        dst = name + '.mp4'
-        os.rename(filename, dst)
+    path = os.getcwd()
+    os.startfile(path)
 
-    elif filename.endswith('.avi'):
-        dst = name + '.avi'
-        os.rename(filename, dst)
+def rename_files():
+    path = os.getcwd()
+    filenames =  os.listdir(path)
+    name = movie.get()
 
-    elif filename.endswith('.mkv'):
-        dst = name + '.mkv'
-        os.rename(filename, dst)
+    for filename in filenames: # for loop searches for key file extensions and renames according to user input
+
+        if filename.endswith('.srt'):
+            dst = name + '.srt'
+            os.rename(filename, dst)
+
+        elif filename.endswith('.mp4'):
+            dst = name + '.mp4'
+            os.rename(filename, dst)
+
+        elif filename.endswith('.avi'):
+            dst = name + '.avi'
+            os.rename(filename, dst)
+
+        elif filename.endswith('.mkv'):
+            dst = name + '.mkv'
+            os.rename(filename, dst)
+
+    
 
 
-os.chdir(path)
-os.rename(hashed_name,name)
+ogpath = make_folder
+root = Tk()
 
-print("The following files have been renamed:")
-print(os.listdir())
+root.title("pyname")
+root.geometry("300x100")
+
+label1 = Label(root, text = "Enter the name of the movie:")
+movie = Entry(root)
+createfolderbutton = Button(root, text = "Create Folder and Open", command=make_folder)
+label2 = Label(root, text = "Move media and select")
+renamebutton = Button(root, text = "This Button", command=rename_files)
+
+label1.grid(row=0,column=0)
+movie.grid(row=0,column=1)
+createfolderbutton.grid(row=1,column=0)
+
+label2.grid(row=5,column=0)
+renamebutton.grid(row=5,column=1)
+
+root.mainloop()
