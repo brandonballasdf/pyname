ef250475addeebaaa47b1d521a731ef02aa5832e
brandonballasdf <35648735+brandonballasdf@users.noreply.github.com>
2018-09-12 11:14:28 -0400

    Add files via upload
    Added an exception case for if you are attempting to create a folder in a directory where that folder exists.

--- pyname.py
+++ pyname.py
@@ -1,57 +1,59 @@
-import os, imp
-import subprocess
+import os
 from tkinter import *
-import tkinter.simpledialog as simpledialog
-
+from tkinter.messagebox import showerror
 
 def make_folder():
-    name_of_movie = movie.get()
-    path = os.getcwd()
+    error = "This directory already exists"
+    name_of_movie = movie.get() # gets value from tkinter entry box
+    path = os.getcwd() # saves current working directory
 
-    os.mkdir(os.path.join(path, name_of_movie)) # make new directory
+    try:
+        os.mkdir(os.path.join(path, name_of_movie)) # make new directory
+    except:
+        showerror("D'Oh", message=str(error))
+
     os.chdir(os.path.join(path, name_of_movie)) # navigate into new directory
 
-    path = os.getcwd()
-    os.startfile(path)
+    path = os.getcwd() # update working directory
+    os.startfile(path) # opens folder created
 
 def rename_files():
-    path = os.getcwd()
-    filenames =  os.listdir(path)
-    name = movie.get()
+    path = os.getcwd() # saves current working directory
+    filenames =  os.listdir(path) #saves filenames in directory as tuple
+    name = movie.get() # gets value from tkinter entry box
 
     for filename in filenames: # for loop searches for key file extensions and renames according to user input
 
-        if filename.endswith('.srt'):
+        if filename.endswith('.srt'): # renames subtitle file
             dst = name + '.srt'
             os.rename(filename, dst)
 
-        elif filename.endswith('.mp4'):
+        elif filename.endswith('.mp4'): # renames .mp4 file
             dst = name + '.mp4'
             os.rename(filename, dst)
 
-        elif filename.endswith('.avi'):
+        elif filename.endswith('.avi'): # renames .avi file
             dst = name + '.avi'
             os.rename(filename, dst)
 
-        elif filename.endswith('.mkv'):
+        elif filename.endswith('.mkv'): # renames .mkv file
             dst = name + '.mkv'
             os.rename(filename, dst)
 
-    os.chdir('..')
+    os.chdir('..') # backs out to root media folder, back to back folders made become nested if this is taken out
 
+root = Tk() # init tkinter
 
-root = Tk()
+root.title("pyname") # title bar name
+root.geometry("300x100") # window size
 
-root.title("pyname")
-root.geometry("300x100")
+menubar = Menu(root) # init tkinter menubar
+root.config(menu=menubar) # set as menubar
 
-menubar = Menu(root)
-root.config(menu=menubar)
+filemenu = Menu(menubar) # init file menu cascade from menubar
+menubar.add_cascade(label='File', menu=filemenu) #  file menu
 
-filemenu = Menu(menubar)
-menubar.add_cascade(label='File', menu=filemenu)
-
-filemenu.add_command(label='Quit', command=sys.exit)
+filemenu.add_command(label='Quit', command=sys.exit) # file menu quit command
 
 
 label1 = Label(root, text = "Enter the name of the movie:")
